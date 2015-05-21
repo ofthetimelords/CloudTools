ef606a04d5f143d3d7b10e194882b5186b960310
Ofthetimelords <ofthetimelords@gmail.com>
2015-05-21 08:11:23 +0300

    Added Version references

--- TheQ.Utilities.CloudTools/Properties/AssemblyInfo.cs
+++ TheQ.Utilities.CloudTools/Properties/AssemblyInfo.cs
@@ -52,8 +52,8 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("4.0.1.0")]
-[assembly: AssemblyFileVersion("4.0.1.0")]
+[assembly: AssemblyVersion("4.0.2.0")]
+[assembly: AssemblyFileVersion("4.0.2.0")]
 [assembly: CLSCompliant(false)]
 [assembly:
 	InternalsVisibleTo(
