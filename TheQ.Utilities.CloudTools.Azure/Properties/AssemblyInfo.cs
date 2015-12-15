de2b97a295b2ee371e5b8bb4b2938687555bdfa5
James Kavakopoulos <ofthetimelords@gmail.com>
2015-12-15 06:33:17 +0200

    Updated packages and versioning

--- TheQ.Utilities.CloudTools.Azure/Properties/AssemblyInfo.cs
+++ TheQ.Utilities.CloudTools.Azure/Properties/AssemblyInfo.cs
@@ -50,7 +50,7 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("7.0.1.0")]
-[assembly: AssemblyFileVersion("7.0.1.0")]
-[assembly: AssemblyInformationalVersion("4.3.0.0")]
+[assembly: AssemblyVersion("7.1.0.0")]
+[assembly: AssemblyFileVersion("7.1.0.0")]
+[assembly: AssemblyInformationalVersion("6.2.0.0")]
 [assembly: CLSCompliant(false)]
